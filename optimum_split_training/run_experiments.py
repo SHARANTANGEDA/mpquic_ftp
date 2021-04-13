a15edb35545cbc85c738a5a3382ec2d8de591af6
Sai Sharan Tangeda <saisarantangeda@gmail.com>
2021-04-13 13:57:01 +0530

    Machine wise Dataset split
    Signed-off-by: Sai Sharan Tangeda <saisarantangeda@gmail.com>

--- optimum_split_training/run_experiments.py
+++ optimum_split_training/run_experiments.py
@@ -1,25 +1,37 @@
-import random
+import argparse
+
+import pandas as pd
 
 import mininet_utils
 import utils
 
+parser = argparse.ArgumentParser(description='Run Experiments Based on Machine ID')
+parser.add_argument('--m_id', type=int, dest="m_id", help="Machine Id", default=0)
+args = parser.parse_args()
+
+dataset_split = args.m_id
+
+df = None
+if dataset_split == 1:
+    df = pd.read_csv("part_1.csv")
+elif dataset_split == 2:
+    df = pd.read_csv("part_2.csv")
+elif dataset_split == 3:
+    df = pd.read_csv("part_3.csv")
+elif dataset_split == 4:
+    df = pd.read_csv("part_4.csv")
+else:
+    df = pd.read_csv("combinations.csv")
+
 path_1, path_2, max_cnt, split_ratios_list, runs_per_combination = utils.load_config()
 
-combinations = utils.generate_all_combinations(path_1['bandwidth_list'], path_1["delay_list"], path_1["loss_list"],
-                                               path_2['bandwidth_list'], path_2["delay_list"], path_2["loss_list"])
-
-df = utils.save_combinations_as_df(combinations)
-
-# Limit Combinations with max_cnt
-random.shuffle(combinations)
-stripped_list = combinations[:max_cnt]
-
 # Train and record split ratio for each combination
-results, total_cnt, current_cnt = [], len(split_ratios_list) * len(stripped_list), 0
-for idx, row in enumerate(stripped_list):
+results, total_cnt, current_cnt = [], len(split_ratios_list) * len(df), 0
+for idx, row in df.iterrows():
     for split_ratio in split_ratios_list:
-        mininet_utils.run_exp_for_combination(row[0], row[1], row[2], row[3], row[4], row[5], split_ratio,
+        mininet_utils.run_exp_for_combination(row['path_1_bw'], row['path_1_delay'], row['path_1_loss'],
+                                              row['path_2_bw'], row['path_2_delay'], row['path_2_loss'], split_ratio,
                                               runs_per_combination)
         current_cnt += 1
         print("Progress: {}/{}; {}% <==> {}/{}".format(current_cnt, total_cnt, current_cnt * 100 / total_cnt,
-                                                       idx, len(stripped_list)))
+                                                       idx, len(df)))

2cf9c6f43a5f1ce8cfc9bff3d2073369420b89b5
Sai Sharan Tangeda <saisarantangeda@gmail.com>
2021-04-20 23:24:43 +0530

    Fix auto testing setup
    Signed-off-by: Sai Sharan Tangeda <saisarantangeda@gmail.com>

--- optimal_split_testing/run_auto_test.py
+++ optimal_split_testing/run_auto_test.py
@@ -7,8 +7,8 @@ df = pd.read_csv("sample_test.csv")
 # Train and record split ratio for each combination
 results, total_cnt, current_cnt = [], len(df), 0
 for idx, row in df.iterrows():
-    mininet_utils.run_exp_for_combination(row['path_1_bw'], row['path_1_delay'], row['path_1_loss'],
-                                          row['path_2_bw'], row['path_2_delay'], row['path_2_loss'])
+    mininet_utils.run_exp_for_combination(row['path_1_bw'], row['delay_1'], row['loss_1'],
+                                          row['path_2_bw'], row['delay_2'], row['loss_2'])
     current_cnt += 1
     print("Progress: {}/{}; {}% <==> {}/{} ".format(current_cnt, total_cnt, current_cnt * 100 / total_cnt, idx,
                                                     len(df)))

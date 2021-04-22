b9128c35196e9d00429db77d6d50c67681dbb8bb
Sai Sharan Tangeda <saisarantangeda@gmail.com>
2021-04-22 14:54:58 +0530

    Retry for all samples
    Signed-off-by: Sai Sharan Tangeda <saisarantangeda@gmail.com>

--- optimum_split_training/convert_experiments_to_dataset.py
+++ optimum_split_training/convert_experiments_to_dataset.py
@@ -10,18 +10,20 @@ results_df = pd.DataFrame(columns=["path_1_bw", "delay_1", "loss_1", "path_2_bw"
                                    "transfer_time"])
 path_1_col, delay_1_col, loss_1_col, path_2_col, delay_2_col, loss_2_col, split_ratios_list,  tr_col = [], [], [], [], \
                                                                                                        [], [], [], []
+scheduler_col = []
 samples = os.listdir(f'./experiments/')
 samples.remove(".gitkeep")
 # samples.remove(".DS_Store")
 for idx, case in enumerate(samples):
-    avg_transfer_time, avail_cnt, excep_cnt = 0, 0, 0
+    avg_transfer_time, avail_cnt, excep_cnt, split_ratio = 0, 0, 0, 0
     for i in range(0, runs_per_combination):
         file = open(f'./experiments/{case}/server_{i}.txt', "r")
         content = file.readlines()
         try:
-            transfer_time = content[7].split(":")[1].strip().split(" ")[0]
+            transfer_time = content[8].split(":")[1].strip().split(" ")[0]
             avg_transfer_time += float(transfer_time)
             avail_cnt += 1
+            split_ratio = content[4].split(":")[1].strip()
         except:
             if avail_cnt == 0 and excep_cnt > 0:
                 print(content, case, i)
@@ -34,10 +36,11 @@ for idx, case in enumerate(samples):
     path_2_col.append(details[3])
     delay_2_col.append(details[4][:len(details[4]) - 2] if "ms" in details[4] else details[4])
     loss_2_col.append(details[5])
-    split_ratios_list.append(details[6])
+    split_ratios_list.append(split_ratio)
+    scheduler_col.append(details[6])
     tr_col.append(avg_transfer_time)
 
 results_df['path_1_bw'], results_df['delay_1'], results_df['loss_1'], results_df['path_2_bw'], results_df['delay_2'],\
 results_df['loss_2'] = path_1_col, delay_1_col, loss_1_col, path_2_col, delay_2_col, loss_2_col
-results_df['split_ratio'], results_df['transfer_time'] = split_ratios_list, tr_col
+results_df['split_ratio'], results_df['transfer_time'], results_df['scheduler'] = split_ratios_list, tr_col, scheduler_col
 results_df.to_csv("./intermediate_results.csv", index=False)
